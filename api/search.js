2c63547e1e0abe21ffd7eff8310171b4f8d6822e
YugaHashi <hashiguchiyuga@gmail.com>
2025-07-04 23:48:02 +0900

    Update search.js

--- api/search.js
+++ api/search.js
@@ -1,19 +1,22 @@
-import { createClient } from '@supabase/supabase-js'
+import { createClient } from '@supabase/supabase-js';
 
 const supabase = createClient(
   process.env.SUPABASE_URL,
   process.env.SUPABASE_SECRET_KEY
-)
+);
 
 export default async function handler(req, res) {
   const query = req.query.q || "";
 
   const { data, error } = await supabase
     .from("find_menus")
-    .select("*")
+    .select("name_jp, description_jp, image_url")
     .ilike("name_jp", `%${query}%`)
-    .limit(1);
+    .limit(5);
 
-  if (error) return res.status(500).json({ error: error.message });
+  if (error) {
+    return res.status(500).json({ error: error.message });
+  }
+
   res.status(200).json(data);
 }
