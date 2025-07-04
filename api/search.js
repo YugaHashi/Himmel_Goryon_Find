f66f3812d4347b5718f5fa8be835b82ff2c675ec
YugaHashi <hashiguchiyuga@gmail.com>
2025-07-04 23:11:49 +0900

    Create search.js

---
+++ api/search.js
@@ -0,0 +1,19 @@
+import { createClient } from '@supabase/supabase-js'
+
+const supabase = createClient(
+  process.env.SUPABASE_URL,
+  process.env.SUPABASE_SECRET_KEY
+)
+
+export default async function handler(req, res) {
+  const query = req.query.q || "";
+
+  const { data, error } = await supabase
+    .from("find_menus")  // ← テーブル名に合わせて変更済み
+    .select("*")
+    .ilike("name_jp", `%${query}%`)
+    .limit(1);
+
+  if (error) return res.status(500).json({ error: error.message });
+  res.status(200).json(data);
+}
